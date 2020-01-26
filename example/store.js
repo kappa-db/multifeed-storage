72b56c9a72528a9bf0a77e5db87ca63b927bc6cb
substack <substack@bits.coop>
2020-01-26 12:34:25 -1000

    update example for the callback api

--- example/store.js
+++ example/store.js
@@ -6,21 +6,23 @@ require('mkdirp').sync('/tmp/multifeed-storage')
 var s = Storage(function (name) {
   return raf(path.join('/tmp/multifeed-storage', name))
 })
-var feed = s.createLocal('hello')
+s.createLocal('hello', function (err, feed) {
+  if (err) return console.error(err)
 
-s.fromDiscoveryKey(feed.discoveryKey, function (err, key) {
-  // key === feed.key
-  console.log('from discovery key:', key.toString('hex'))
-})
-
-s.fromLocalName('hello', function (err, key) {
-  // key === feed.key
-  console.log('from local name:', key.toString('hex'))
-})
-
-// append data, close the feed, and open it again with get():
-feed.append('whatever', function (err) {
-  s.close(feed.key)
-  var feed1 = s.get(feed.key)
-  feed1.get(0, console.log)
+  s.fromDiscoveryKey(feed.discoveryKey, function (err, key) {
+    // key === feed.key
+    console.log('from discovery key:', key.toString('hex'))
+  })
+  s.fromLocalName('hello', function (err, key) {
+    // key === feed.key
+    console.log('from local name:', key.toString('hex'))
+  })
+  // append data, close the feed, and open it again with get():
+  feed.append('whatever', function (err) {
+    s.close(feed.key)
+    s.get(feed.key, function (err, feed1) {
+      if (err) console.error(err)
+      else feed1.get(0, console.log)
+    })
+  })
 })
